e425d07c233fb0174cbcfa36545b0c74b8fa28c6
cbatist4 <cbatist4@ncsu.edu>
2025-01-27 20:45:19 -0500

    updated comments

--- webassembliss/emulation/arm64_linux.py
+++ webassembliss/emulation/arm64_linux.py
@@ -3,10 +3,17 @@ from typing import List, Dict
 from io import BytesIO
 from qiling import Qiling
 
-# Register the NZCV register into qiling's arm64 register map.
+# Register the NZCV register into qiling's arm64 register map so we can read status bits.
+# This was tricky to find... but here are the references in case you need to do the same:
+# First, official documentation saying where condition codes are: https://developer.arm.com/documentation/ddi0601/2024-12/AArch64-Registers/NZCV--Condition-Flags
+# Unfortunately, qiling.arm64const does not link to the nzcv register: https://github.com/qilingframework/qiling/blob/master/qiling/arch/arm64_const.py
+# It creates a RegManager with the registers above but also links the an unicorn object: https://github.com/qilingframework/qiling/blob/9a78d186c97d6ff42d7df31155dda2cd9e1a7fe3/qiling/arch/arm64.py#L42
+# The unicorn object points to the UC_ARCH_ARM64: https://github.com/qilingframework/qiling/blob/9a78d186c97d6ff42d7df31155dda2cd9e1a7fe3/qiling/arch/arm64.py#L23-L24
+# From the unicorn project, we can see nzcv in the register list: https://github.com/unicorn-engine/unicorn/blob/d568885d64c89db5b9a722f0c1bef05aa92f84ca/bindings/python/unicorn/arm64_const.py#L16
 import qiling.arch.arm64_const
 from unicorn.arm64_const import UC_ARM64_REG_NZCV
 
+# Update the register map with our new entry.
 qiling.arch.arm64_const.reg_map.update({"nzcv": UC_ARM64_REG_NZCV})
 
 ROOTFS_PATH = "/webassembliss/rootfs/arm64_linux"
@@ -16,15 +23,7 @@ LD_CMD = "aarch64-linux-gnu-ld"
 
 def get_nzcv(ql: Qiling) -> Dict[str, bool]:
     """Parses the NZCV condition codes from the given qiling instance."""
-    # This was tricky to find... but here are the references in case you need to do the same:
-    # First, official documentation saying where condition codes are: https://developer.arm.com/documentation/ddi0601/2024-12/AArch64-Registers/NZCV--Condition-Flags
-    # Unfortunately, qiling.arm64const does not link to the nzcv register: https://github.com/qilingframework/qiling/blob/master/qiling/arch/arm64_const.py
-    # It creates a RegManager with the registers above but also links the an unicorn object: https://github.com/qilingframework/qiling/blob/9a78d186c97d6ff42d7df31155dda2cd9e1a7fe3/qiling/arch/arm64.py#L42
-    # The unicorn object points to the UC_ARCH_ARM64: https://github.com/qilingframework/qiling/blob/9a78d186c97d6ff42d7df31155dda2cd9e1a7fe3/qiling/arch/arm64.py#L23-L24
-    # From the unicorn project, we can see nzcv in the register list: https://github.com/unicorn-engine/unicorn/blob/d568885d64c89db5b9a722f0c1bef05aa92f84ca/bindings/python/unicorn/arm64_const.py#L16
-    # If we try reading that key from RegMap there's an error: ql.arch.regs.read("UC_ARM64_REG_NZCV") -> KeyError: 'uc_arm64_reg_nzcv'
-    # So we can try manually doing it: https://github.com/qilingframework/qiling/blob/9a78d186c97d6ff42d7df31155dda2cd9e1a7fe3/qiling/arch/register.py#L60
-    # From the unicorn project we can see that nzcv is register #3 from their list: https://github.com/unicorn-engine/unicorn/blob/d568885d64c89db5b9a722f0c1bef05aa92f84ca/bindings/python/unicorn/arm64_const.py#L16
+    # Ref: https://developer.arm.com/documentation/ddi0601/2024-12/AArch64-Registers/NZCV--Condition-Flags
     nzcv = ql.arch.regs.read("nzcv")
     return {
         "N": bool(nzcv & (1 << 31)),
