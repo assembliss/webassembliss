80786c532cb4136628e9af173be2c7c45344d9c6
cbatist4 <cbatist4@ncsu.edu>
2025-01-27 20:32:45 -0500

    added nzcv register to supported arch regs

--- webassembliss/emulation/arm64_linux.py
+++ webassembliss/emulation/arm64_linux.py
@@ -1,8 +1,12 @@
 from .utils import clean_emulation, EmulationResults
 from typing import List, Dict
 from io import BytesIO
-from qiling.arch.arm64_const import reg_map
 from qiling import Qiling
+
+# Register the NZCV register into qiling's arm64 register map.
+import qiling.arch.arm64_const
+from unicorn.arm64_const import UC_ARM64_REG_NZCV
+qiling.arch.arm64_const.reg_map.update({"nzcv": UC_ARM64_REG_NZCV})
 
 ROOTFS_PATH = "/webassembliss/rootfs/arm64_linux"
 AS_CMD = "aarch64-linux-gnu-as"
@@ -20,7 +24,7 @@ def get_nzcv(ql: Qiling) -> Dict[str, bool]:
     # If we try reading that key from RegMap there's an error: ql.arch.regs.read("UC_ARM64_REG_NZCV") -> KeyError: 'uc_arm64_reg_nzcv'
     # So we can try manually doing it: https://github.com/qilingframework/qiling/blob/9a78d186c97d6ff42d7df31155dda2cd9e1a7fe3/qiling/arch/register.py#L60
     # From the unicorn project we can see that nzcv is register #3 from their list: https://github.com/unicorn-engine/unicorn/blob/d568885d64c89db5b9a722f0c1bef05aa92f84ca/bindings/python/unicorn/arm64_const.py#L16
-    nzcv = ql.arch.regs.uc.reg_read(3)
+    nzcv = ql.arch.regs.read("nzcv")
     return {
         "N": bool(nzcv & (1 << 31)),
         "Z": bool(nzcv & (1 << 30)),
@@ -46,7 +50,7 @@ def emulate(
     if ld_flags is None:
         ld_flags = ["-o"]
     if registers is None:
-        registers = list(reg_map.keys())
+        registers = list(qiling.arch.arm64_const.reg_map)
 
     # Run the emulation and return its status and results.
     return clean_emulation(
