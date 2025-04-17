28338264f8896270d40c7b3af9a1d2a0bf0267c4
cbatist4 <cbatist4@ncsu.edu>
2025-04-17 11:38:02 -0400

    Bytes -> B when displaying sizes

--- webassembliss/static/js/base_editor.js
+++ webassembliss/static/js/base_editor.js
@@ -713,7 +713,7 @@ function formatHumanSize(bytes, decimals) {
     if (bytes == 0) return '0 Bytes';
     var k = 1024,
         dm = decimals || 2,
-        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
         i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
