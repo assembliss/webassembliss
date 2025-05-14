83ca95c848be128a76306dd1bd60716c2b7fa31e
cbatist4 <cbatist4@ncsu.edu>
2025-05-14 10:50:01 -0400

    remove post method for project builder form

--- webassembliss/app.py
+++ webassembliss/app.py
@@ -56,15 +56,8 @@ def grader():
     return render_template("grader.html.j2")
 
 
-@app.route("/project-config-builder/", methods=["POST", "GET"])
+@app.route("/project-config-builder/")
 def project_config_builder():
-    if request.method == "POST":
-        # If POST, make sure we got here from the submission form.
-        if request.referrer != request.url:
-            abort(403)
-        abort(403)
-
-    # If not POST, show the submission form.
     return render_template("project_config_builder.html.j2")
 
 
